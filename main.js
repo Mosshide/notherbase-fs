04f10fb3c4f8e479c2ec4586261b2977371dab0c
wsushinsky <wyattsushi@gmail.com>
2021-09-06 22:47:13 -0700

    initial

---
+++ main.js
@@ -0,0 +1,43 @@
+const router = require("express").Router();
+let dir;
+
+let explore = function explore(route, styles = [], scripts = []) {
+    styles = styles.map(style => {
+        style = `${dir}/styles/${style}`;
+        return style;
+    });
+
+    scripts = scripts.map(script => {
+        script = `${dir}/local-scripts/${script}`;
+        return script;
+    });
+
+    let main = "index";
+    if (route !== "") main = route;
+
+    router.get(`/${route}`, async function(req, res) {
+        try {
+            res.render(`explorer`, 
+            {
+                siteTitle: "NotherBase",
+                user: null,
+                styles: styles,
+                main: `${dir}/views/${main}`,
+                scripts: scripts
+            });
+        }
+        catch(err) {
+            console.log(err);
+        }
+    });
+}
+
+let from = function from(currentDirectory) {
+    dir = currentDirectory;
+}
+
+module.exports = {
+    explore: explore,
+    from: from,
+    router: router
+}
