052d16226a4b05c85afc6bf5342de6d5b22f0135
wsushinsky <wyattsushi@gmail.com>
2022-01-24 04:29:26 -0800

    mongoose connect fix

--- models/start-mongoose.js
+++ models/start-mongoose.js
@@ -4,9 +4,7 @@ const mongoose = require("mongoose");
 //connect mongoose and mongo
 mongoose.connect(process.env.MONGODB_URI, { 
 	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useCreateIndex: true,
-	useFindAndModify: false,
+	useUnifiedTopology: true
 });
 
 //handlers
