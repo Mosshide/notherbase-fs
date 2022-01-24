2d62a6a590f378f19dcbec34cb2fab8ea8a4e40c
wsushinsky <wyattsushi@gmail.com>
2022-01-24 03:19:11 -0800

    models

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 module.exports = {
-    chat: require("chat"),
-    item: require("item"),
+    chat: require("./chat"),
+    item: require("./item"),
     portfolio: require("./portfolio/portfolio-controller.js"),
-    user: require("user"),
-    authCheck: require("authCheck"),
-    contact: require("contact"),
-    explorer: require("explorer"),
-    front: require("the-front"),
-    inventory: require("inventory")
+    user: require("./user"),
+    authCheck: require("./authCheck"),
+    contact: require("./contact"),
+    explorer: require("./explorer"),
+    front: require("./the-front"),
+    inventory: require("./inventory")
 }
--- models/index.js
+++ models/index.js
@@ -1,11 +1,10 @@
 module.exports = {
     chat: require("./chat"),
-    item: require("item"),
-    user: require("user"),
-    contact: require("contact"),
-    explorer: require("explorer"),
-    front: require("the-front"),
-    inventory: require("inventory"),
+    item: require("./item"),
+    user: require("./user"),
+    contact: require("./contact"),
+    explorer: require("./explorer"),
+    inventory: require("./inventory"),
 }
 
 require("start-mongoose");
