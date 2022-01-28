484e07c51da6902343323043e65ac908b0199bd9
wsushinsky <wyattsushi@gmail.com>
2022-01-27 19:55:18 -0800

    added test and fixed inventory bug

---
+++ test/test-index.js
@@ -0,0 +1,50 @@
+const notherbase = require("../index");
+
+const world = {
+    explorer: {
+        name: "explorer",
+        dirname: __dirname,
+        options: {},
+        void: "void",
+        regions: [
+            {
+                name: "coast",
+                dirname: __dirname,
+                options: {},
+                areas: [
+                    {
+                        name: "tall-beach",
+                        dirname: __dirname,
+                        options: {},
+                        pois: [
+                            {
+                                name: "nono-cove",
+                                dirname: __dirname,
+                                options: {},
+                                details: [
+                                    {
+                                        name: "",
+                                        options: {}
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    theFront: {
+        name: "the-front",
+        dirname: __dirname,
+        options: {},
+        details: [
+            {
+                name: "",
+                options: {}
+            }
+        ]
+    }
+};
+
+notherbase.start(world);
