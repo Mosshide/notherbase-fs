64804f6d4fc58f65adf3465cd2bcde744ad82add
wsushinsky <wyattsushi@gmail.com>
2022-06-04 00:16:11 -0700

    password change removed from menu

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
-const nodemailer = require("nodemailer");
 
 // Import my Data
 const { user, inventory, sendMail } = require("../models");
