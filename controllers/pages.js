ea8e66714e12ce311fdf01c7132a1dc3033bfb9f
wsushinsky <wyattsushi@gmail.com>
2022-10-16 20:04:20 -0700

    pages server scripts update

--- controllers/pages.js
+++ controllers/pages.js
@@ -2,66 +2,20 @@ const express = require("express");
 const router = express.Router();
 const fs = require('fs');
 
-const page = require("../models").page;
+const db = require("../models");
 
 module.exports = function name(path)
 {
     let files = fs.readdirSync(path);
 
-    router.get('/pages/db', async function (req, res) {
+    router.post(`/serve/:script`, async function(req, res) {
         try {
-            if (req.query.type === "global") {
-                let foundPage = await page.findOne({name: req.query.name, type: "global"});
-        
-                if (!foundPage) res.status(404).end();
-                else res.status(200).send(foundPage);
-            }
-            else {
-                let foundPage = await page.findOne({name: req.query.name, user: req.session.currentUser});
-        
-                if (!foundPage) res.status(404).end();
-                else res.status(200).send(foundPage);
-            }
+            let scriptResult = await require(`${path}/scripts/${req.params.script}.js`)(db, req.session.currentUser, req.body);
+            res.send(scriptResult);
         }
         catch(err) {
-            res.status(500).end();
             console.log(err);
-        }
-    });
-
-    router.post('/pages/db', async function (req, res) {
-        try {
-            let foundPage;
-
-            if (req.query.type === "global") {
-                foundPage = await page.findOne({name: req.query.name, type: "global"});
-            }
-            else {
-                foundPage = await page.findOne({name: req.query.name, user: req.session.currentUser});
-            }
-            
-            if (!foundGame) {
-                await game.create({
-                    name: req.body.name,
-                    type: req.query.type,
-                    user: req.session.currentUser,
-                    data: req.query.data
-                });
-    
-                res.status(200).end();
-            } 
-            else {
-                foundGame.data = req.query.data;
-                foundGame.markModified("data");
-                await foundGame.save();
-    
-                res.status(200).end();
-            }
-        }
-        catch(err) {
             res.status(500).end();
-            console.log(req.query);
-            console.log(err);
         }
     });
 
