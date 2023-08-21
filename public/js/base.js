17790b77c408eef358b1005ab7cea34a110d628e
wsushinsky <wyattsushi@gmail.com>
2023-08-20 20:25:15 -0700

    no more menu

--- public/js/base.js
+++ public/js/base.js
@@ -14,108 +14,6 @@ class Base {
     }
 
     /**
-     * The player's inventory.
-     */
-    #Inventory = class Inventory {
-        constructor() {
-            this.items = [];
-            this.lastUpdate = 0;
-    
-            this.refresh();
-        }
-        
-        /**
-         * Reloads the inventory.
-         */
-        async refresh() {
-            let $list = $(".inventory .item-list");
-
-            let response = await Base.commune("getInventory", { _lastUpdate: this.lastUpdate });
-
-            if (response.status === "success") {
-                this.items = response.data;
-                this.lastUpdate = response.lastUpdate;
-                $list.empty();
-                
-                for (let i = 0; i < this.items.length; i++) {
-                    let $new = $list.append(
-                        `<div class="item-card">
-                            <h5>${this.items[i].name}</h5>
-                            <button id="${i}">X</button>
-                            <hr>
-                            <p>${this.items[i].amount}</p>
-                        </div>`
-                    ).children().last();
-        
-                    $new.find("button").on("click", this.reduceItem);
-                }
-
-                this.clearError();
-            }
-        }
-        
-        /**
-         * Clears the error on screen.
-         */
-        clearError() {
-            let $error = $("#inventory #error");
-
-            $error.addClass("invisible");
-        }
-    
-        /**
-         * Shows an error on screen.
-         * @param {String} text The error message.
-         */
-        setError(text) {
-            let $error = $("#inventory #error");
-
-            $error.text(text);
-            $error.removeClass("invisible");
-        }
-    }
-
-    /**
-     * The player's attributes.
-     */
-    #PlayerAttributes = class PlayerAttributes {
-        constructor() {
-            this.attributes = [];
-            this.lastUpdate = 0;
-
-            this.refresh();
-        }
-
-        /**
-         * Reloads the player's attributes.
-         */
-        async refresh() {
-            let response = await Base.commune("getAttributes", { _lastUpdate: this.lastUpdate });
-            if (response.status === "success") {
-                this.lastUpdate = response.lastUpdate;
-                this.attributes = response.data;
-    
-                this.render();
-            }
-        }
-
-        /**
-         * Renders the attributes.
-         */
-        render() {
-            let $content = $(".menu .content#player");
-
-            $content.empty();
-
-            if (this.attributes) {
-                for (const [key, value] of Object.entries(this.attributes)) {
-                    $content.append(`<h3 id="${key}">${key}: ${value}</h3>`);
-                }
-            }
-        }
-    }
-
-    /**
      * Services for the player's account.
      */
     #AccountServices = class AccountServices {
@@ -125,56 +23,6 @@ class Base {
             this.lastUpdate = 0;
             
             this.refresh();
-        }
-    
-        /**
-         * Reloads the player's basic info.
-         */
-        async refresh() {
-            let response = await Base.commune("getInfo", { _lastUpdate: this.lastUpdate });
-            if (response.status === "success") {
-                this.lastUpdate = response.lastUpdate;
-                this.email = response.data.email;
-                this.username = response.data.username;
-            }
-
-            let $email = $(".content#account .setting#email p");
-            let $emailInput = $(".content#account .edit#email input");
-            let $username = $(".content#account .setting#username p");
-            let $usernameInput = $(".content#account .edit#username input");
-            
-            $email.text(this.email);
-            $emailInput.val(this.email);
-            $username.text(this.username);
-            $usernameInput.val(this.username);
-    
-            $(".content#account .settings").removeClass("invisible");
-            $(".content#account #please-login").addClass("invisible");
-        }
-    
-        /**
-         * Initiates email editing.
-         */
-        editEmail() {
-            let $emailSetting = $(".content#account .setting#email");
-            let $emailEdit = $(".content#account .edit#email");
-
-            $emailSetting.addClass("invisible");
-            $emailEdit.removeClass("invisible");
-        }
-    
-        /**
-         * Cancels editing the email.
-         */
-        cancelEmail() {
-            let $email = $(".content#account .setting#email p");
-            let $emailSetting = $(".content#account .setting#email");
-            let $emailEdit = $(".content#account .edit#email");
-            let $emailInput = $(".content#account .edit#email input");
-
-            $emailSetting.removeClass("invisible");
-            $emailEdit.addClass("invisible");
-            $emailInput.val($email.text());
         }
     
         /**
@@ -196,31 +44,6 @@ class Base {
                 $info.text("Email Not Updated!");
             }
             this.cancelEmail();
-        }
-    
-        /**
-         * Initiates username editing.
-         */
-        editUsername() {
-            let $usernameSetting = $(".content#account .setting#username");
-            let $usernameEdit = $(".content#account .edit#username");
-
-            $usernameSetting.addClass("invisible");
-            $usernameEdit.removeClass("invisible");
-        }
-    
-        /**
-         * Cancels username editing.
-         */
-        cancelUsername() {
-            let $usernameSetting = $(".content#account .setting#username");
-            let $usernameEdit = $(".content#account .edit#username");
-            let $usernameInput = $(".content#account .edit#username input");
-            let $username = $(".content#account .setting#username p");
-
-            $usernameSetting.removeClass("invisible");
-            $usernameEdit.addClass("invisible");
-            $usernameInput.val($username.text());
         }
     
         /**
@@ -246,56 +69,9 @@ class Base {
     }
     
     constructor() {
-        this.playerInventory = new this.#Inventory();
-        this.playerAttributes = new this.#PlayerAttributes();
         this.playerAccount = new this.#AccountServices();
-        this.menuClosing = false;
-
-        this.switchTab("inventory");
     }
 
-    /**
-     * Closes the menu.
-     */
-    closeMenu = () => {
-        let $menu = $(".ui .menu");
-        let $fade = $(".ui .fade");
-
-        if (!this.menuClosing) {
-            this.menuClosing = true;
-            $fade.addClass("camo");
-            
-            setTimeout(() => { 
-                $menu.addClass("invisible");
-                $fade.addClass("invisible");
-                this.menuClosing = false;
-            }, 100);
-        }
-    }
-
-    /**
-     * Opens the menu.
-     */
-    openMenu = () => {
-        let $menu = $(".ui .menu");
-        let $fade = $(".ui .fade");
-
-        $menu.removeClass("invisible");
-        $fade.removeClass("camo");
-        $fade.removeClass("invisible");
-    }
-
-    /**
-     * Switches tabs in the menu.
-     * @param {String} id The name of the tab to switch to.
-     */
-    switchTab = function switchTab(id) {
-        $("#content-window .content").addClass("invisible");
-        $(".menu .tabs button").removeClass("selected");
-        $(`#content-window #${id}`).removeClass("invisible");
-        $(`.menu .tabs #${id}`).addClass("selected");
-    }
-    
     /**
      * Communes to logout.
      * @returns Communion response.
