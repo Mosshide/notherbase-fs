72e9d6115869d4e3bef80a8937814ca535ac2b41
Wyatt Sushinsky <wyattsushi@gmail.com>
2024-11-11 21:05:17 -0800

    proxx

--- notherbase-fs.js
+++ notherbase-fs.js
@@ -41,7 +41,7 @@ class NotherBaseFS {
         }));
 
         //be safe, needs to be before session
-        if (process.env.PRODUCTION == "true") this.app.set('trust proxy', 2);
+        if (process.env.PRODUCTION == "true") this.app.set('trust proxy', 3);
 
         let baseKeys = Object.keys(this.bases);
         let store = MongoStore.create({ mongoUrl: process.env.MONGODB_URI });
